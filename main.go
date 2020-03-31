94791189aba12db20ccb76eaa0286373aa8b3623
Julian Poschmann <julian.poschmann@lancom.de>
2020-03-31 16:29:04 +0200

    Cleanup handlers

--- main.go
+++ main.go
@@ -24,21 +24,17 @@ func main() {
 	flag.Parse()
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/toldyouso", ToldYouSoHandler)
-	http.HandleFunc("/health", HealthHandler)
+	http.HandleFunc("/toldyouso", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		} else {
+			toldYouSo.Inc()
+		}
+	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "OK")
+	})
 
 	log.Printf("Starting webserver on \"%s\"", *listenAddress)
 	log.Fatal(http.ListenAndServe(*listenAddress, nil))
-}
-
-func ToldYouSoHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	} else {
-		toldYouSo.Inc()
-	}
-}
-
-func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "OK")
 }
